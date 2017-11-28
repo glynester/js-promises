3163b5cbf680bd30a181dcc314dd31b24794d029
glynester <glynester@hotmail.com>
2017-11-28 15:09:15 +0000

    Changes to data returned.

--- index.js
+++ index.js
@@ -25,22 +25,49 @@ function http(url, method){
 
 // My github id: 20479118
 
+
 console.log("Fetching users...");
+var user, repo;
 http("https://api.github.com/users?since=20479117", "GET")
+
 	.then(function(users){
 		console.log("Fetching repos...");
-		var user = users[0];
+		user = users[0];
 		return http(user.repos_url, "GET");
 	})
 	.then(function(repos){
-		repos.forEach(r=>{
-			console.log(r.name);
-			}
-		)
+		console.log("Fetching languages...");
+		repo = repos[1];
+		return http(repo.languages_url, "GET");
+	})
+	.then(function(langs){
+		console.log(`USER: ${user.login} , REPO: ${repo.name}, LANGUAGES: ${JSON.stringify(langs)}`);
 	})
 	.catch(function(err){
 		console.log("Error caught: ", err);
 	});
 
 
-	
+
+
+// console.log("Fetching users...");
+// http("https://api.github.com/users?since=20479117", "GET")
+// 	.then(function(users){
+// 		console.log("Fetching repos...");
+// 		var user = users[0];
+// 		return http(user.repos_url, "GET");
+// 	})
+// 	.then(function(repos){
+
+// 		var rep = {};
+// 		repos.forEach(r=>{
+// 			rep[r.name] = r.language;
+// 			}
+// 		)
+// 		console.log(rep);
+// 	})
+// 	.catch(function(err){
+// 		console.log("Error caught: ", err);
+// 	});
+
+
