ff0c30ea1c07ec09643960ef6c8ec25b8d043c41
glynester <glynester@hotmail.com>
2017-11-28 16:21:03 +0000

    Variations on a theme

--- index.js
+++ index.js
@@ -26,48 +26,83 @@ function http(url, method){
 // My github id: 20479118
 
 
-console.log("Fetching users...");
-var user, repo;
-http("https://api.github.com/users?since=20479117", "GET")
-
-	.then(function(users){
+// var user, repo;
+var user = "glynester", repo="djangogirls",
+url = "https://api.github.com/users/" + user;
+console.log("Fetching user...");
+http(url, "GET")
+	.then(function(user){
 		console.log("Fetching repos...");
-		user = users[0];
 		return http(user.repos_url, "GET");
 	})
 	.then(function(repos){
-		console.log("Fetching languages...");
-		repo = repos[1];
-		return http(repo.languages_url, "GET");
+		repoArr = [];
+		var repoObj;
+		repos.forEach(r=>{
+			repoObj = {};
+			repoObj[r.name] = r.language;
+			repoArr.push(repoObj);
+			}
+		)
+		return repoArr;
 	})
-	.then(function(langs){
-		console.log(`USER: ${user.login} , REPO: ${repo.name}, LANGUAGES: ${JSON.stringify(langs)}`);
+	.then(function(reps){
+		var langs;
+		reps.forEach(r=>{
+			if (Object.keys(r)[0] === repo){
+				langs = Object.values(r)[0];
+			}
+		});
+		console.log(`The language(s) used by "${user}" on the "${repo}" project are: ${langs}`);
 	})
 	.catch(function(err){
 		console.log("Error caught: ", err);
 	});
 
-
-
-
+// var user, repo;
 // console.log("Fetching users...");
 // http("https://api.github.com/users?since=20479117", "GET")
 // 	.then(function(users){
 // 		console.log("Fetching repos...");
-// 		var user = users[0];
+// 		user = users[0];
 // 		return http(user.repos_url, "GET");
 // 	})
 // 	.then(function(repos){
 
-// 		var rep = {};
+// 		repo = {};
 // 		repos.forEach(r=>{
-// 			rep[r.name] = r.language;
+// 			repo[r.name] = r.language;
 // 			}
 // 		)
-// 		console.log(rep);
+// 		console.log(repo);
 // 	})
 // 	.catch(function(err){
 // 		console.log("Error caught: ", err);
 // 	});
+
+
+// console.log("Fetching users...");
+// var user, repo;
+// http("https://api.github.com/users?since=20479117", "GET")
+
+// 	.then(function(users){
+// 		console.log("Fetching repos...");
+// 		user = users[0];
+// 		return http(user.repos_url, "GET");
+// 	})
+// 	.then(function(repos){
+// 		console.log("Fetching languages...");
+// 		repo = repos[1];
+// 		return http(repo.languages_url, "GET");
+// 	})
+// 	.then(function(langs){
+// 		console.log(`USER: ${user.login} , REPO: ${repo.name}, LANGUAGES: ${JSON.stringify(langs)}`);
+// 	})
+// 	.catch(function(err){
+// 		console.log("Error caught: ", err);
+// 	});
+
+
+
 
 
